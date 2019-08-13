e8a51f7168b0feea0c82463a35a84d88a2ac8378
Kenia Rioja <kdrioja@gmail.com>
2019-08-13 15:19:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.exampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Creates and sets up the main activity in our app
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
